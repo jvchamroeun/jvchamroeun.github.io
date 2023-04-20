a15bee4998c7c312318d35f54a83e27db65e618d
Jason Soukchamroeun <j.soukchamroeun@gmail.com>
2023-04-19 22:20:50 -0700

    Responsive header txt update

--- js/custom-elements/main.js
+++ js/custom-elements/main.js
@@ -70,8 +70,8 @@ class MyHeader extends HTMLElement {
                 <div class="row">
                     <div class="col-lg-8 col-md-10 mx-auto">
                         <div class="page-heading">
-                            <h1 style="font-size:60px; text-shadow: 2px 0 #000, -2px 0 #000, 0 2px #000, 0 -2px #000,
-                            1px 1px #000, -1px -1px #000, 1px -1px #000, -1px 1px #000;">Jason S. Portfolio</h1>
+                            <h1 style="text-shadow: 2px 0 #000, -2px 0 #000, 0 2px #000, 0 -2px #000,
+                            1px 1px #000, -1px -1px #000, 1px -1px #000, -1px 1px #000;">Jason Soukchamroeun Portfolio</h1>
                             <span class="subheading">Network Security Application and Software Developer well versed in SQL, Python, React, React Native and more...</span>
                         </div>
                     </div>
